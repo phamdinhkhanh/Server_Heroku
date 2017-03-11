ffccc45c75fd7c4908fff87ed41b3238230c9111
laptopTCC-PC\laptopTCC <phamdinhkhanh>
2017-03-12 02:45:11 +0700

    setting token key for server login

--- app.py
+++ app.py
@@ -3,37 +3,42 @@ import mlab
 from models.task import Task
 from flask_restful import Api
 from resources.task_resource import *
-
+from mongoengine import Document, StringField
+from flask_jwt import JWT,jwt_required
 mlab.connect()
+#Flask co chuc nang tao framework cho app
 app = Flask(__name__)
 api = Api(app)
-
-# task = Task(name = "Khanh Pham", gender = "Male",age = 24,local_id="ajdhd-ạhfhh-dsah",color = "#2FE6G9",done = False)
-# task.save()
-
-# for i in range(10):
-#     task = Task(name = "Khanh Pham Dinh"+str(i), local_id = "adbca-adafr-123"+str(i),color = "#2DF4H"+str(i))
-#     task.save()
-
+app.config["SECRET_KEY"] = "MY SECRET KEY"
 
 all_tasks = Task.objects()
 for task in all_tasks:
     print(mlab.item2json(task))
 
+class LoginCredential:
+    def __init__(self,id):
+        self.id = id
 
 
-# my_task = Task.objects(name = "Khanh Pham").first()
-# print(mlab.item2json(my_task))
+class User(Document):
+    username = StringField();
+    password = StringField();
 
+# user = User(user="admin",password="admin")
+# user.save()
 
-# my_task.update(set__done = True)
+def authenticate(username, password):
+    for user in User.objects().filter(username = username):
+       if user.password == password:
+            return LoginCredential(str(user.id))
 
+def identity(payload):
+     payload['identity']
 
-# for khanh in Task.objects():
-#     khanh.delete()
-
+jwt = JWT(app,authenticate,identity)
 
 @app.route('/')
+@jwt_required()
 def hello_world():
     return 'Hello World!'
 
